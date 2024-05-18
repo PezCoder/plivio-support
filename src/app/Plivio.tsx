a1e037c71da2cf803a6e8eb767fb176f2fc2175b
Rahul Gupta <rgupta2@atlassian.com>
2024-05-18 06:54:39 +0530

    Add basic redix buttons

--- src/app/Plivio.tsx
+++ src/app/Plivio.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button"
 import {useEffect, useState} from "react";
 import {createUser, getUsers} from "./backend/api";
+import {Home} from "lucide-react";
+import Link from "next/link";
 
 export const Plivio = () => {
   const [user, setUser] = useState('');
@@ -20,5 +23,18 @@ export const Plivio = () => {
     })();
   }, []);
 
-  return <div>testing</div>
+  return <div>
+    <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+      hello
+      <Link
+        href="#"
+        className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+      >
+        <Home className="h-4 w-4" />
+        Dashboard
+      </Link>
+    </nav>
+    <h1>heading</h1>
+    <Button>Testing button</Button>
+  </div>
 };
